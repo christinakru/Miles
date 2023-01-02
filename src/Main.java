1c067e8c8ac3d5bdd7b1b9e0f1d7449b3f7d82ba
Kristina Krutova <krutovaerws@mail.ru>
2023-01-02 20:17:07 +0300

    Use local variable instead of const

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,15 @@
 import java.util.Scanner;
 
 public class Main {
-    final static int RublesPerMiles = 20;
     public static void main(String[] args) {
+        int rublesPerMiles = 20;
+
         System.out.println("Введите стоимость билета:");
 
         Scanner in = new Scanner(System.in);
         int cost = in.nextInt();
 
-        int miles = cost / RublesPerMiles;
-        System.out.println(String.format("Вы получаете %d миль!", miles));
+        int miles = cost / rublesPerMiles;
+        System.out.printf("Вы получаете %d миль!%n", miles);
     }
 }
